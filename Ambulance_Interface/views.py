14debfc7d80454a52c59c2e7ffcf91f18aa69025
qiross <lucasqreis@poli.ufrj.br>
2024-01-27 00:10:09 -0300

    Link da interface com API de voz e API de geolocalização

--- Ambulance_Interface/views.py
+++ Ambulance_Interface/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
+import requests
+import magic
+from pydub import AudioSegment
 from django.http import JsonResponse
 
 def record_audio(request):
     if request.method == 'POST' and request.FILES.get('audio'):
         audio_file = request.FILES['audio']
-        with open('uploaded_audio.wav', 'wb') as f:
-            for chunk in audio_file.chunks():
-                f.write(chunk)
-        return JsonResponse({'message': 'Audio uploaded successfully'})
-    else:
-        return render(request, 'record_audio.html')
+
+        audio_type = magic.from_buffer(audio_file.read(), mime=True)
+        if audio_type != 'video/webm':
+            return JsonResponse({'error': 'Invalid audio file format. Expected: video/webm'}, status=400)
+
+        audio_file.seek(0)
+
+        audio = AudioSegment.from_file(audio_file, format='webm')
+        wav_data = audio.export(format='wav')
+
+        transcribe_url = 'http://127.0.0.1:5000/transcribe'
+        files = {'file': ('uploaded_audio.wav', wav_data)}
+        response = requests.post(transcribe_url, files=files)
+
+        if response.status_code == 200:
+            second_response = requests.get('http://127.0.0.1:2947')
+
+            if second_response.status_code == 200:
+                return JsonResponse({
+                    'transcription': response.json(),
+                    'second_response_data': second_response.json()
+                })
+            else:
+                return JsonResponse({
+                    'transcription': response.json(),
+                    'error': 'Failed to fetch data from 127.0.0.1:2947'
+                }, status=500)
+        else:
+            return JsonResponse({'error': 'Failed to transcribe audio'}, status=500)
+
+    return render(request, 'record_audio.html')
